3d5ea757beb47c7878cc6519dd048bcfe25da95f
Chiara <chiarafioref@gmail.com>
2026-05-30 00:37:54 +0200

    implement basic ATM withdrawal logic with balance validation

--- 16/main.js
+++ 16/main.js
@@ -1,10 +1,11 @@
 console.log("connesso");
 
 let textbalance = prompt("Qual è il tuo saldo attuale?");
-let balance = Number(textmoney);
+let balance = Number(textbalance);
 
 if (balance >= 0) {
-    let money = prompt("Quale importo vuoi prelevare?")
+    let textmoney = prompt("Quale importo vuoi prelevare?")
+    let money = Number(textmoney)
 } if (balance >= money) {
     console.log("Prelievo effettuato. " + "Saldo aggiornato: " + (balance - money))
 } else {
